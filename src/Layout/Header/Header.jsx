b7fa22cac2fa60a62e5db344c8bcd46c21f82f5a
JavohirMuradov <javohirmuradov030@gmail.com>
2024-03-17 12:44:25 +0500

    ready

---
+++ src/Layout/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+            <div> 
+
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
